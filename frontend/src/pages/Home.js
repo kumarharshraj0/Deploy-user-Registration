3ab004b6aa22423be838907d66495bc45cbb929f
kumarharshraj0 <harshsawarn2@gmail.com>
2025-08-01 14:54:43 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = https://deploy-user-registration.vercel.app/products";
+            const url = "https://deploy-user-registration.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
